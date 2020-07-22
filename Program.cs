2cf21a1f2ed9650fa1961f3939b6694704e6d48b
Elias Molla <elias.molla.cel@gmail.com>
2020-07-21 23:18:56 -0300

    Primer commit

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace NotasApi
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+        
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // if (env.IsDevelopment())
+            // {
+            //     app.UseDeveloperExceptionPage();
+            // }
+
+            //app.UseHttpsRedirection();
+
+            app.UseRouting();
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthorization();
+        }
+    }
+}
